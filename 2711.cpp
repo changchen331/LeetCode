103bae8b09783ce34701974803d56e1b977a0375
ChangCheng <changeagle@163,com>
2025-03-26 02:23:25 +0800

    2025/3/26

--- 2711.cpp
+++ 2711.cpp
@@ -9,29 +9,57 @@ public:
     vector<vector<int>> differenceOfDistinctValues(vector<vector<int>> &grid)
     {
         int size = grid.size(), length = grid[0].size();
-        vector<vector<int>> answer(size);
+        vector<vector<int>> answer(size, vector<int>(length));
+        unordered_set<int> count;
 
-        for (int x = 0; x < size; x++)
+        // 方法一
+        // int left, right, xx, yy;
+        // for (int x = 0; x < size; x++)
+        // {
+        //     for (int y = 0; y < length; y++)
+        //     {
+        //         // 左
+        //         count.clear();
+        //         xx = x - 1, yy = y - 1;
+        //         while (xx >= 0 && yy >= 0)
+        //             count.insert(grid[xx--][yy--]);
+        //         left = count.size();
+
+        //         // 右
+        //         count.clear();
+        //         xx = x + 1, yy = y + 1;
+        //         while (xx < size && yy < length)
+        //             count.insert(grid[xx++][yy++]);
+        //         right = count.size();
+
+        //         answer[x][y] = abs(left - right);
+        //     }
+        // }
+
+        // 方法二
+        // k = length - y + x => x = k + y - length => y = length - k + x
+        // 1 <= k <= length + (size - 1)
+        for (int k = 1; k < (size + length); k++)
         {
-            for (int y = 0; y < length; y++)
+            int start_y = max(length - k + 0, 0);
+            int end_y = min(length - k + (size - 1), length - 1);
+
+            // left
+            count.clear();
+            for (int y = start_y; y <= end_y; y++)
             {
-                int left = 0, right = 0, xx = x - 1, yy = y - 1;
-                unordered_set<int> count;
+                int x = k + y - length;
+                answer[x][y] = count.size();
+                count.insert(grid[x][y]);
+            }
 
-                // 左
-                while (xx >= 0 && yy >= 0)
-                    count.insert(grid[xx--][yy--]);
-                left = count.size();
-
-                xx = x + 1, yy = y + 1;
-                count.clear();
-
-                // 右
-                while (xx < size && yy < length)
-                    count.insert(grid[xx++][yy++]);
-                right = count.size();
-
-                answer[x].push_back(abs(left - right));
+            // right
+            count.clear();
+            for (int y = end_y; y >= start_y; y--)
+            {
+                int x = k + y - length;
+                answer[x][y] = abs(answer[x][y] - (int)count.size());
+                count.insert(grid[x][y]);
             }
         }
 
