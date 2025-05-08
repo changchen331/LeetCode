8af06c42dce77b2ffba078070732564bd2d88a4e
ChangCheng <changeagle@163,com>
2025-05-08 21:15:50 +0800

    2025/5/8

--- 1123.cpp
+++ 1123.cpp
@@ -25,46 +25,31 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-// 树的初始化
-TreeNode *init(vector<int> values)
-{
-    TreeNode root = TreeNode(values[0]);
-    int size = values.size();
-
-    queue<TreeNode *> nodes;
-    nodes.push(&root);
-    for (int i = 1; i < size; i += 2)
-    {
-        TreeNode *node = nodes.front();
-        nodes.pop();
-
-        if (values[i] >= 0)
-            nodes.push(node->left = new TreeNode(values[i]));
-        if (i + 1 < size && values[i + 1] >= 0)
-            nodes.push(node->right = new TreeNode(values[i + 1]));
-    }
-
-    return &root;
-}
-
-// 展示树结构
-
 class Solution
 {
+private:
+    pair<int, TreeNode *> dfs(TreeNode *node)
+    {
+        if (node == nullptr)
+        {
+            return {0, nullptr};
+        }
+        auto [left_height, left_lca] = dfs(node->left);
+        auto [right_height, right_lca] = dfs(node->right);
+        if (left_height > right_height)
+        { // 左子树更高
+            return {left_height + 1, left_lca};
+        }
+        if (left_height < right_height)
+        { // 右子树更高
+            return {right_height + 1, right_lca};
+        }
+        return {left_height + 1, node}; // 一样高
+    }
+
 public:
     TreeNode *lcaDeepestLeaves(TreeNode *root)
     {
-        return root;
+        return dfs(root).second;
     }
 };
-
-int main(int argc, char const *argv[])
-{
-    Solution solution;
-    vector<int> values{3, 5, 1, 6, 2, 0, 8, -1, -1, 7, 4};
-    TreeNode *root = init(values);
-
-    TreeNode *answer = solution.lcaDeepestLeaves(root);
-
-    return 0;
-}
